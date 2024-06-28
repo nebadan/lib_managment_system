5a8409621d69721f0d07fe47cc9c31dd04d350d3
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-06-28 18:16:50 +0300

    chore: Update database password in application.properties

--- src/main/java/com/itsc/library_managment_system/lib_managment_system/controller/BookController.java
+++ src/main/java/com/itsc/library_managment_system/lib_managment_system/controller/BookController.java
@@ -1,7 +1,5 @@
 package com.itsc.library_managment_system.lib_managment_system.controller;
 
-
-
 import com.itsc.library_managment_system.lib_managment_system.model.Book;
 import com.itsc.library_managment_system.lib_managment_system.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +17,7 @@ public class BookController {
 
   @GetMapping
   public List<Book> getAllBooks() {
+    System.out.println("getAll books called");
     return bookService.getAllBooks();
   }
 
@@ -28,7 +27,9 @@ public class BookController {
   }
 
   @PostMapping
+
   public Book createBook(@RequestBody Book book) {
+    System.out.println("create book called");
     return bookService.saveBook(book);
   }
 
